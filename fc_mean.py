c714523b843a17c03b2b661ab0fcd0566881e835
mhmdsmdi <mhmd.samadi76@gmail.com>
2019-05-01 16:01:51 +0430

    All Plot Add

--- fc_mean.py
+++ fc_mean.py
@@ -5,17 +5,34 @@ import matplotlib as mpl
 FUZZINESS_PARAMETER = 2.00
 
 
-def point_generator(feature_size, num_data):
-    x1 = np.random.uniform(0, 40, (feature_size, num_data / 2))
-    x2 = np.random.uniform(-40, 0, (feature_size, num_data / 2))
-    # x3 = np.random.uniform(-40, 40, (feature_size, num_data / 2))
-    # x3[1:, ] = x3[1:, ] * -1
-    # x4 = np.random.uniform(-40, 0, (feature_size, num_data / 4))
-    # x4[1:, ] = x4[1:, ] * -1
-    x1 = np.append(x1, x2, axis=1)
-    # x2 = np.append(x3, x4, axis=1)
-    # x1 = np.append(x1, x3, axis=1)
-    return x1
+def point_generator(num_cluster, feature_size, num_data):
+    data = np.zeros((feature_size, 0))
+    cof = 1
+    # for i in range(num_cluster):
+
+    # x1 = np.random.beta(a, b, (feature_size, num_data / num_cluster)) * 2000
+    # x1[0, :] = np.random.choice([1, -1, 2, -2]) * x1[0, :] + np.random.randint(-1000, 1000)
+    # x1[1, :] = np.random.choice([1, -1, 2, -2]) * x1[1, :] + np.random.randint(-1000, 1000)
+    # cof += 1
+    # data = np.append(x1, data, axis=1)
+
+    a = np.random.randint(10, 15)
+    b = np.random.randint(8, 12)
+    x1 = np.random.beta(a, b, (feature_size, num_data / num_cluster)) * 500
+    x1[0, :] = 2 * x1[0, :] + 300
+    x1[1, :] = x1[1, :]
+
+    x2 = np.random.beta(a, b, (feature_size, num_data / num_cluster)) * 500
+    x2[0, :] = 2 * x2[0, :]
+    x2[1, :] = x2[1, :] + 250
+
+    x3 = np.random.beta(a, b, (feature_size, num_data / num_cluster)) * 500
+    x3[0, :] = 2 * x3[0, :] - 300
+    x3[1, :] = x3[1, :]
+
+    data = np.append(x1, x2, axis=1)
+    data = np.append(data, x3, axis=1)
+    return data
 
 
 def calculate_c_center(U, X):
@@ -47,19 +64,20 @@ def destination(xk, vi):
     return np.linalg.norm(xk - vi)
 
 
-def fcmean(data, num_clusters=2, num_iteration=100):
-    X = point_generator(2, 10000)
-    U = np.random.uniform(0, 1, (X.shape[1], num_clusters))
+def fcmean(data, num_clusters=3, num_iteration=100):
+    U = np.random.uniform(0, 1, (data.shape[1], num_clusters))
     U = U / U.sum(axis=0, keepdims=1)
-    cluster_centers = None
-    i = num_iteration
-    while i > 0:
-        i -= 1
-        cluster_centers = calculate_c_center(U, X)
-        U = update_membership_value(X, cluster_centers)
-    labels = np.argmax(U, axis=1)
-    print(labels)
-    show_result(X, labels, cluster_centers, num_iteration)
+    cluster_centers = labels = None
+    i = 0
+    while i != num_iteration:
+        cluster_centers = calculate_c_center(U, data)
+        U = update_membership_value(data, cluster_centers)
+        labels = np.argmax(U, axis=1)
+        if i in [2, 5, 10, 20, 100]:
+            show_result(data, labels, cluster_centers, i)
+        i += 1
+    print(labels.shape)
+    show_result(data, labels, cluster_centers, num_iteration)
 
 
 def show_result(X, labels, cluster_centers, num_iteration):
@@ -67,22 +85,51 @@ def show_result(X, labels, cluster_centers, num_iteration):
     y_data = X[1, :]
     x_centroid = cluster_centers[0, :]
     y_centroid = cluster_centers[1, :]
-    txt = "(Cluster = {}, Iteration = {}, Data = {}, FuzzyParameter = {})".format(cluster_centers.shape[1], num_iteration, X.shape[1], FUZZINESS_PARAMETER)
-    cmap = plt.cm.jet
-    cmaplist = [cmap(i) for i in range(cmap.N)]
-    cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
-    bounds = np.linspace(0, cluster_centers.shape[1], cluster_centers.shape[1] + 1)
-    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+    txt = "(Cluster = {}, Iteration = {}, Data = {}, FuzzyParameter = {})".format(cluster_centers.shape[1],
+                                                                                  num_iteration, X.shape[1],
+                                                                                  FUZZINESS_PARAMETER)
+    # cmap = plt.cm.jet
+    # cmaplist = [cmap(i) for i in range(cmap.N)]
+    # cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
+    # bounds = np.linspace(0, cluster_centers.shape[1], cluster_centers.shape[1] + 1)
+    # norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
     fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-    scat = ax.scatter(x_data, y_data, s=30, c=labels, cmap=cmap, norm=norm)
-    cb = plt.colorbar(scat, spacing='proportional', ticks=bounds)
-    cb.set_label('Clusters')
-    ax.set_title("FCM Algorithm\n" + txt)
+    # scat = ax.scatter(x_data, y_data, s=30, c=labels)#, cmap=cmap, norm=norm)
+    # cb = plt.colorbar(scat, spacing='proportional', ticks=bounds)
+    # cb.set_label('Clusters')
+    for i in range(len(labels)):
+        label = labels[i]
+        if label == 0:
+            plt.plot(x_data[i], y_data[i], 'ob')
+        if label == 1:
+            plt.plot(x_data[i], y_data[i], 'or')
+        if label == 2:
+            plt.plot(x_data[i], y_data[i], 'og')
+    # ax.set_title("FCM Algorithm\n" + txt)
+    # ax.set_xlabel('X-axis')
+    # ax.set_ylabel('Y-axis')
+    plt.ylabel("Y-axis")
+    plt.xlabel("X-axis")
+    plt.title("FCM Algorithm\n" + txt)
+    plt.scatter(x_centroid, y_centroid, s=400, marker='+', c="yellow")
+    plt.show()
+
+
+def show_input_data(X):
+    x_data = X[0, :]
+    y_data = X[1, :]
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    ax.set_title("FCM Algorithm\n Input Data")
     ax.set_xlabel('X-axis')
     ax.set_ylabel('Y-axis')
-    plt.scatter(x_centroid, y_centroid, s=400, marker='+', c="red")
+    scat = ax.scatter(x_data, y_data, s=30)
     plt.show()
 
 
 if __name__ == '__main__':
-    fcmean(100)
+    num_cluster = 3
+    feature_size = 2
+    num_data = 1500
+    X = point_generator(num_cluster=num_cluster, feature_size=feature_size, num_data=num_data)
+    show_input_data(X)
+    fcmean(X)
